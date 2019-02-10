37956157d1dd29e40057c2be8130339717677cef
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 16:48:48 -0400

    print words that are not in word list

--- Lab4_Task4.py
+++ Lab4_Task4.py
@@ -9,5 +9,19 @@ def histogram(filename):
   line = line.split()
   for word in line:
    word = word.strip (string.punctuation + string.whitespace).lower()
-   hist[word] = hist.get(word, 0)+1
+   hist[word] = hist.get(word, 0)
  return(hist)
+
+def difference(a,b):
+    text = set(a)
+    word = set(b)
+    diff = text.difference(word)
+
+    print("text set:", text)
+    print("word set:", word)
+    print("words in the book that are not in the word list:")
+    print("different set:", diff)
+   
+file1 = histogram("mybook.txt")
+file2 = histogram("words.txt")
+difference(file1, file2)
