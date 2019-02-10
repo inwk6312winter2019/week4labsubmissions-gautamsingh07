062cc4eff76cc91ac835474b95ef66b2587dc9d5
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 17:08:09 -0400

    file String Replacement

--- Lab4_Task7.py
+++ Lab4_Task7.py
@@ -7,3 +7,17 @@ def remove(word):
   str = word.strip() 
   str = word.replace(p,'')
  return str
+
+
+def sed(file1,file2):
+ try:
+  f1 = open(filename1,"r")
+  f2 = open(filename2,"w")
+   for char in f1 :
+    new = removepattern(char)
+    f2.write(cgar)
+   print("File Modified")
+ except:
+  print("Operation Failed")
+
+
