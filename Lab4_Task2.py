e09c83852b10ef947a5055fddfcc0e79cc184a43
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 16:25:18 -0400

    count the words of both files

--- Lab4_Task2.py
+++ Lab4_Task2.py
@@ -12,8 +12,27 @@ for line in file1:
        print(word.strip(string.punctuation))
        count = count +1
        if word not in d:
-         d[word] = 1
+         d1[word] = 1
        else:
-         d[word] += 1
+         d1[word] += 1
 
 print(count)
+
+
+file2 = open("58809-0.txt")
+for line in file2:
+    line = line.strip()
+    line = line.lower()
+    line = line.split()
+    for word in line:
+       print(word.strip(string.punctuation))
+       count = count +1
+       if word not in d1:
+          d2[word] = 1
+       else:
+          d2[word] += 1
+print(count)
+
+print(len(d))
+print(len(d1))
+
