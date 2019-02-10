6c1a757173f4b41888f9b847397e6378011c9795
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 17:20:11 -0400

    move rectangle

--- Lab5_Task2.py
+++ Lab5_Task2.py
@@ -9,4 +9,15 @@ class Rectangle():
         self.width = width
         self.corner = corner
 
-  
+def centre(self):
+    self.x = self.corner.x + (self.height / 2)
+    self.y = self.corner.y + (self.width / 2)
+    return (self.x, self.y)
+
+
+p = int(input("enter x coordinates of corner: "))
+q = int(input("enter y coordinates of corner: "))
+height = int(input("enter the Height: "))
+width = int(input("enter the width: "))
+box = Rectangle(height, width, corner=Point(p, q))
+print(box.centre())  
