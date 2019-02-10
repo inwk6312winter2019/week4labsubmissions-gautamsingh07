a645200235e4365a345a4619a07c495240708e23
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 16:30:53 -0400

    print 20 most used words

---
+++ Lab4_Task3.py
@@ -0,0 +1,44 @@
+import string
+
+count = 0
+d1 =dict()
+d2 =dict()
+file1 = open("58820-0.txt")
+for line in file1:
+    line = line.strip()
+    line = line.lower()
+    line = line.split()
+    for word in line:
+       print(word.strip(string.punctuation))
+       count = count +1
+       if word not in d:
+         d1[word] = 1
+       else:
+         d1[word] += 1
+
+print(count)
+
+
+file2 = open("58809-0.txt")
+for line in file2:
+    line = line.strip()
+    line = line.lower()
+    line = line.split()
+    for word in line:
+       print(word.strip(string.punctuation))
+       count = count +1
+       if word not in d1:
+          d2[word] = 1
+       else:
+          d2[word] += 1
+print(count)
+
+print(len(d))
+print(len(d1))
+
+for char in d1.keys():
+  if d1[k]>20:
+    print(k)
+for char in d2.keys():
+  if d2[k]>20:
+    print(k)
