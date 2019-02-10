222ce88c51d54ccfdba284a4a5ef0122fd8d9c66
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 16:56:25 -0400

    walk function

---
+++ Lab4_Task6.py
@@ -0,0 +1,11 @@
+import os
+
+def walk(dir):
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        if os.path.isfile(path):
+            print path
+        else:
+            walk(path)
+
