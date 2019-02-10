8bc492f0b7f4679f8f377b68e85f0b5d73707cf8
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 17:55:36 -0400

    Time Format

--- Lab5_Task3.py
+++ Lab5_Task3.py
@@ -1,6 +1,11 @@
 class time:
-	def __init__(self, time):
-		self.minutes, self.seconds = divmod(time, 60)
-		self.hours, self.minutes = divmod(self.minutes, 60)
-		print(self.hours, self.minutes, self.seconds)
+ def __init__(self, time):
+	self.minutes, self.seconds = divmod(time, 60)
+	self.hours, self.minutes = divmod(self.minutes, 60)
+	print(self.hours, self.minutes, self.seconds)
 
+ def __str__(self):
+	return ('Time= %.2d:%.2d:%.2d')%(self.hours,self.minutes,self.seconds)
+time1 = time(1234)
+
+print(time1)
