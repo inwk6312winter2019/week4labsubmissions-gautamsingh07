828d3a7348a178f76d399762fb48e14b131ad932
Gautam Singh <nonu@Gautams-MacBook-Air.local>
2019-02-10 21:33:07 -0400

    Add method for Points

---
+++ Lab5_Task8.py
@@ -0,0 +1,27 @@
+class Point():
+ def __init__(self,x = 0, y = 0):
+	self.x = x
+	self.y = y
+ def __str__(self):
+	return "String"
+ def __add__(self, other):
+	point_add = Point()
+	if (type(other) == tuple):
+		point_add.x = self.x + other[0]
+		point_add.y = self.y + other[1]
+		return point_add
+	else:
+		point_add.x = self.x + other.x
+		point_add.y = self.y + other.y
+		return point_add
+
+point1 = Point(4,3)
+point2 = (6,4)
+
+print(point1.__str__())
+
+new = point1.__add__(point2)
+
+print(new.x)
+print(new.y)
+
